f032e29da48aefe9ed2bfb4de8d64c0b711f8e1b
Winrida <winridas@gmail.com>
2020-09-02 10:16:25 +0300

    Initial commit

---
+++ src/ru/mirea/lishchenko/pr1/HelloJava.java
@@ -0,0 +1,8 @@
+package ru.mirea.lishchenko.pr1;
+import java.lang.System;
+import java.util.*;
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
